abf1131b9f439b88dc1093c79f768e5a6279f3b0
Benjamin Held <me@bheld.eu>
2017-03-07 19:23:22 +0100

    added script to build fortran bindings

---
+++ wrf_install/wrf_preparation/fortran_bindings.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# @Author: Benjamin Held
+# @Date:   2017-02-18 15:39:54
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2017-03-07 19:22:24
+
+# define terminal colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHT_BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Script to compile the required netcdf package
+
+# Jump in folder and extract tar
+cd ~/$1
+printf "${YELLOW}\nUnpacking netcdf-fortran.tar files: ${NC}\n"
+tar -xf netcdf-fortran-4.4.4.tar.gz
+
+# Build netcdf-fortran bindings
+cd netcdf-fortran-4.4.4
+# Change the path according to the used user; configure requires an absolute
+# path here or it fails with an error
+CPPFLAGS="-I$DIR/netcdf/include" ./configure --prefix=$NETCDF --disable-shared
+make -j 2 && make -j 2 install
+
+cd ..
+
+printf "${LIGHT_BLUE}\nFinished installing netcdf fortran bindings. ${NC}\n"
