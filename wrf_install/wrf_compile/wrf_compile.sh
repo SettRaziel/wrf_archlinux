5d1ad280992523dfa27e5f3fc6d6ef823c3527a6
Benjamin Held <me@bheld.eu>
2017-03-13 20:57:07 +0100

    adjusted wrf_compile script

--- wrf_install/wrf_compile/wrf_compile.sh
+++ wrf_install/wrf_compile/wrf_compile.sh
@@ -2,14 +2,28 @@
 # @Author: Benjamin Held
 # @Date:   2017-02-18 15:49:25
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-03-10 23:50:09
+# @Last Modified time: 2017-03-13 20:56:26
+
+# define terminal colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHT_BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Script to compile the wrf model, after setting up all dependencies
+# and paths
+# $1: the path to the folder where the wrf should be installed
+# $2: the path to the folder where the wrf files has been unpacked
 
 # Jump in folder and extract tar
 cd ~/$1
+printf "${YELLOW}\nUnpacking wrf.tar files: ${NC}\n"
 tar -xf wrf-3.8.1.tar.bz2
 
 # Build wrf
 cd WRFV3
+printf "${YELLOW}\nInstaling wrf: ${NC}\n"
 # Change the path according to the used user; configure requires an absolute
 # path here or it fails with an error
 sudo ln -s /bin/cpp /lib/cpp
@@ -19,3 +33,5 @@ ln -s $2/WRFV3/frame/ $2/WRFV3/external/
 ./compile -j 1 em_real >& ./compile.log
 
 cd ..
+printf "${LIGHT_BLUE}\nFinished installing wrf. ${NC}"
+printf "${LIGHT_BLUE}Check compile.log for details ${NC}\n"
