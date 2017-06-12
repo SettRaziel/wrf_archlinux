3f51cf4fa1bf04f33b7328f3088994810c4ce193
Benjamin Held <me@bheld.eu>
2017-06-12 19:26:10 +0200

    cont. refactoring run_preproccesing

--- wrf_run/model_run/run_preprocessing.sh
+++ wrf_run/model_run/run_preprocessing.sh
@@ -2,13 +2,43 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-12 09:26:31
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-04-28 23:18:38
+# @Last Modified time: 2017-06-12 19:24:21
 
-BUILD_PATH=$1
-GFS_PATH=$2
+# define terminal colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHT_BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-source ./set_env.sh
+BUILD_PATH=${1}
+GFS_PATH=${2}
 
-sh clean_up.sh $BUILD_PATH
+source ../set_env.sh
 
-sh ./pre_processing.sh $BUILD_PATH ${GFS_PATH}
+# cleaning up in wps preprocessing folder
+SCRIPT_PATH=$(pwd)
+now=$(date +"%T")
+printf "Cleaning up wps data from last time at ${now}\n" >> ${SCRIPT_PATH}/../log.info
+cd ${BUILD_PATH}/WPS
+
+# remove met_em files from the last run
+rm met_em.d01.*
+
+# remove grib files
+rm GRIB*
+
+# remove FILE objects of the time stamps
+rm FILE*
+rm PFILE*
+
+# cleaning up in wrf
+now=$(date +"%T")
+printf "Cleaning up wrf data from last time at ${now}\n" >> ${SCRIPT_PATH}/../log.info
+cd ${BUILD_PATH}/WRFV3/test/em_real/
+
+# remove met_em files from the last run
+rm met_em.d01.*
+
+cd ${SCRIPT_PATH}
+sh ./pre_processing.sh ${BUILD_PATH} ${GFS_PATH}
