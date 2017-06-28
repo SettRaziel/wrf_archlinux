da1bbea835980611b207bea426fb3098c1f1534d
Benjamin Held <me@bheld.eu>
2017-06-28 20:36:56 +0200

    update preprocessing doc

--- wrf_run/model_run/pre_processing.sh
+++ wrf_run/model_run/pre_processing.sh
@@ -2,7 +2,10 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-07 19:02:57
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-06-23 20:31:42
+# @Last Modified time: 2017-06-28 20:35:12
+
+# Script to start the model run
+# $1: the path to the wrf root folder
 
 # define terminal colors
 RED='\033[0;31m'
@@ -10,9 +13,6 @@ GREEN='\033[0;32m'
 LIGHT_BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
-
-# Script to run the preprocessing operations
-# $1: the path to the wrf root folder
 
 # logging time stamp
 SCRIPT_PATH=$(pwd)
