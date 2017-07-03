358607070d0818991377b635c10849fad627ab5b
Benjamin Held <me@bheld.eu>
2017-07-03 17:55:26 +0200

    added error handling to main script

--- wrf_run/run_model.sh
+++ wrf_run/run_model.sh
@@ -2,12 +2,20 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-18 09:40:15
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-06-30 20:34:19
+# @Last Modified time: 2017-07-03 17:53:49
 
 # main script for starting a wrf model run
 # Version 0.1.0
 # created by Benjamin Held and other sources, June 2017
 # $1: the starting hour of the model run
+
+function error_exit {
+  now=$(date +"%T")
+  printf "${1} at: ${now}.\n" >> ${SCRIPT_PATH}/error.log
+  printf "Error: ${1} at: ${now}.\n" >> ${STATUS_FILE}
+  echo "${1}" 1>&2
+  exit 1
+}
 
 # imports
 source ${SCRIPT_PATH}/set_env.sh
@@ -16,6 +24,7 @@ source ${SCRIPT_PATH}/set_env.sh
 GFS_PATH=${HOME}/gfs_data
 SCRIPT_PATH=${HOME}/scripts
 BUILD_PATH=${HOME}/Build_WRF
+STATUS_FILE=./status.log      # customize path if required
 PERIOD=180                    # the time period of the forecast
 RESOLUTION="0p50"             # the resolution of the input data
 
@@ -38,15 +47,28 @@ fi
 
 # adjusting namelist for next run
 cd ${SCRIPT_PATH}/model_run
-sh prepare_namelist.sh ${BUILD_PATH} ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}
+printf "Starting namelist preparation.\n" >> ${STATUS_FILE}
+sh prepare_namelist.sh ${BUILD_PATH} ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}; ret=${?}
+if ! [ ${ret} == 0 ]; then
+  error_exit "Failed to prepare the namelist files"
+fi
 
 # fetching input data
 cd ${SCRIPT_PATH}/data_fetch
-sh gfs_fetch.sh "${YEAR}${MONTH}${DAY}" ${HOUR} ${GFS_PATH} ${RESOLUTION} ${PERIOD}
+printf "Starting namelist preparation.\n" >> ${STATUS_FILE}
+sh gfs_fetch.sh "${YEAR}${MONTH}${DAY}" ${HOUR} ${GFS_PATH} ${RESOLUTION} ${PERIOD}; ret=${?}
+if ! [ ${ret} == 0 ]; then
+  error_exit "Failed to prepare the namelist files"
+fi
 
 # start model run
 cd ${SCRIPT_PATH}/model_run
-sh run_preprocessing.sh ${BUILD_PATH} ${GFS_PATH} ${RESOLUTION}
+printf "Starting model run and preparation.\n" >> ${STATUS_FILE}
+sh run_preprocessing.sh ${BUILD_PATH} ${GFS_PATH} ${RESOLUTION}; ret=${?}
+cd ${SCRIPT_PATH}
+if ! [ ${ret} == 0 ]; then
+    error_exit "Failed to run the model"
+fi
 
 # move output files
 cd ${SCRIPT_PATH}/model_run
