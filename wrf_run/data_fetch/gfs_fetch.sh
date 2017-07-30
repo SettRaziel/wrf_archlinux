2804316742265d00dff382a765a6bb91467e1944
Benjamin Held <me@bheld.eu>
2017-07-30 21:58:14 +0200

    extended data fetch - added additional wegt call to reload failed downloads

--- wrf_run/data_fetch/gfs_fetch.sh
+++ wrf_run/data_fetch/gfs_fetch.sh
@@ -2,24 +2,27 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-06 19:18:17
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-06-24 16:58:29
+# @Last Modified time: 2017-07-30 21:57:14
 
-# This script loads the required input data for a specified forecast run
+# This script loads the required input data for a 180 h forecast run
 # $1 matches the required date yyyymmdd
 # $2 matches the required timestamp
 # $3 is the storage path
 # $4 is the model resolution [0p25, 0p50, 1p00]
-# $5 the considered time period
 
 # logging time stamp
-SCRIPT_PATH=$(pwd)
 now=$(date +"%T")
-printf "Starting gfs data fetching at ${now}.\n" > ${SCRIPT_PATH}/../log.info
+printf "Starting gfs data fetching at ${now}.\n" > ./log.info
 
 # Remove old files
 rm ${3}/gfs.*
 
 # Fetch the new ones
-for i in $(seq -f %03g 0 3 ${5}); do
+for i in $(seq -f %03g 0 3 180); do
 wget -q -P ${3} http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs.${1}${2}/gfs.t${2}z.pgrb2.${4}.f${i}
+done
+
+# Check and continue broken files
+for i in $(seq -f %03g 0 3 180); do
+wget -c -q -P ${3} http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs.${1}${2}/gfs.t${2}z.pgrb2.${4}.f${i}
 done
