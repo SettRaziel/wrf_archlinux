d5d817a3f961f2c74f6027acd405ba5b1aede978
Benjamin Held <me@bheld.eu>
2018-01-01 17:52:50 +0100

    added dynamic forecast range

--- wrf_run/data_fetch/gfs_fetch.sh
+++ wrf_run/data_fetch/gfs_fetch.sh
@@ -2,13 +2,14 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-06 19:18:17
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-07-30 21:57:14
+# @Last Modified time: 2018-01-01 17:49:25
 
 # This script loads the required input data for a 180 h forecast run
 # $1 matches the required date yyyymmdd
 # $2 matches the required timestamp
 # $3 is the storage path
 # $4 is the model resolution [0p25, 0p50, 1p00]
+# $5 the time period for the model run
 
 # logging time stamp
 now=$(date +"%T")
@@ -18,11 +19,11 @@ printf "Starting gfs data fetching at ${now}.\n" > ./log.info
 rm ${3}/gfs.*
 
 # Fetch the new ones
-for i in $(seq -f %03g 0 3 180); do
+for i in $(seq -f %03g 0 3 ${5}); do
 wget -q -P ${3} http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs.${1}${2}/gfs.t${2}z.pgrb2.${4}.f${i}
 done
 
 # Check and continue broken files
-for i in $(seq -f %03g 0 3 180); do
+for i in $(seq -f %03g 0 3 ${5}); do
 wget -c -q -P ${3} http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs.${1}${2}/gfs.t${2}z.pgrb2.${4}.f${i}
 done
