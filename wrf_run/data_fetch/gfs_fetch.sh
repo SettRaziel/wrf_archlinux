9abdeba133ae83020bfbe1f9f4c71f6cbddc5446
Benjamin Held <me@bheld.eu>
2018-03-18 21:04:40 +0100

    made data fetch more robust against connection reset

--- wrf_run/data_fetch/gfs_fetch.sh
+++ wrf_run/data_fetch/gfs_fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # @Author: Benjamin Held
 # @Date:   2017-03-06 19:18:17
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-01-23 17:04:09
+# @Last Modified by:   benjamin
+# @Last Modified time: 2018-03-18 21:03:09
 
 # This script loads the required input data for a 180 h forecast run
 # $1 matches the required date yyyymmdd
@@ -13,12 +13,21 @@
 
 # logging time stamp
 now=$(date +"%T")
-printf "Starting gfs data fetching at ${now}.\n" > ./log.info
+printf "Starting gfs data fetch by removing old files at ${now}.\n" > ./log.info
 
 # Remove old files
 rm ${3}/gfs.*
 
-# Fetch the new ones
+printf "Starting gfs data fetching at ${now}.\n" > ./log.info
+# Define a number of retries and try to download the files
 for i in $(seq -f %03g 0 3 ${5}); do
-	curl -o ${3}/gfs.t${2}z.pgrb2.${4}.f${i} http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs.${1}${2}/gfs.t${2}z.pgrb2.${4}.f${i}
+  tries=5
+  while [ $tries -gt 0 ] 
+  do
+    curl -C - -o ${3}/gfs.t${2}z.pgrb2.${4}.f${i} http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/gfs.${1}${2}/gfs.t${2}z.pgrb2.${4}.f${i}
+    if [ $? -ne 56 ]
+     then break
+    fi
+    let --tries
+  done
 done
