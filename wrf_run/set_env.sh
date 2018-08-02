9d74252a70d6008107cff323870e546e0470cfae
Benjamin Held <me@bheld.eu>
2018-08-02 20:25:44 +0200

    added env variables

--- wrf_run/set_env.sh
+++ wrf_run/set_env.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # @Author: Benjamin Held
 # @Date:   2017-03-03 17:20:53
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-07-04 19:54:04
+# @Last Modified by:   benjamin
+# @Last Modified time: 2018-07-28 16:08:09
 
 # Script that sets the required variables for the model run
 # $1: the build path where the required wrf files are installed
 
 # Setting required environment variables for the session
-export DIR="${1}/libraries"
+export BUILD_PATH=${HOME}/Build_WRF
+export DIR="${BUILD_PATH}/libraries"
 export CC="gcc"
 export CXX="g++"
 export FC="gfortran"
