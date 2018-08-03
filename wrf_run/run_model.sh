c7f7eee656cff72436d48895b683b115f3779017
Benjamin Held <me@bheld.eu>
2018-08-03 17:02:16 +0200

    adjusted variables and shell calls

--- wrf_run/run_model.sh
+++ wrf_run/run_model.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-18 09:40:15
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-09-23 13:51:52
+# @Last Modified time: 2018-08-03 17:00:32
 
 # main script for starting a wrf model run
 # Version 0.1.0
@@ -15,19 +15,23 @@ function error_exit () {
   printf "${ERROR_STATUS}\n" >> ${ERROR_LOG}
   printf "Error: ${1} at: ${now}.\n" >> ${STATUS_FILE}
   cd ${SCRIPT_PATH}
+  sh create_mail.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} "${ERROR_STATUS}" "Fail"
   echo "${1}" 1>&2
+  if [ "${RESOLUTION}" != "1p00" ]; then
+    sh run_model.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD} "1p00"
+  fi
   exit 1
 }
 
 # imports
+SCRIPT_PATH=$(pwd)
 source ${SCRIPT_PATH}/set_env.sh
+export LOG_PATH=${SCRIPT_PATH}/logs
 
 # default variables
 GFS_PATH=${HOME}/gfs_data
-SCRIPT_PATH=${HOME}/scripts
-BUILD_PATH=${HOME}/Build_WRF
-STATUS_FILE=./status.log                     # customize path if required
-ERROR_LOG=${SCRIPT_PATH}/error_$(date +"%m_%d").log # path to error log
+STATUS_FILE=${LOG_PATH}/status.log               
+ERROR_LOG=${LOG_PATH}/error_$(date +"%m_%d").log # path to error log
 
 if [ "$#" -eq 3 ]; then # no argument, run whole script
   YEAR=`date '+%Y'`
@@ -48,6 +52,9 @@ else
   echo "Must either be two for <HOUR> <PERIOD> or five for <YEAR> <MONTH> <DAY> <HOUR> <PERIOD>"
   exit 1
 fi
+
+# preparing status file
+printf "Starting new model run for: ${YEAR}/${MONTH}/${DAY} ${HOUR}:00 UTC.\n" > ${STATUS_FILE}
 
 # adjusting namelist for next run
 cd ${SCRIPT_PATH}/model_run
@@ -82,7 +89,7 @@ sh clean_up_output.sh
 mv ${BUILD_PATH}/WRFV3/test/em_real/wrfout_d01_* ${HOME}/wrf_output
 
 # run output script
-cd ${SCRIPT_PATH}
+cd ${SCRIPT_PATH}/post_processing
 printf "Starting postprocessing.\n" >> ${STATUS_FILE}
 sh draw_plots.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}; ret=${?}
 if [ ${ret} == 0 ]; then
@@ -94,4 +101,6 @@ else
     error_exit "Error while creating output files"
 fi
 
+cd ${SCRIPT_PATH}
+sh create_mail.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} "Finished model run without error." "Success"
 printf "Finished model run without error.\n" >> ${STATUS_FILE}
