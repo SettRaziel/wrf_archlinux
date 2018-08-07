71f6a704e3adfc27992ff1184ad833cc008d7435
Benjamin Held <me@bheld.eu>
2018-08-07 19:06:01 +0200

    refactored plotting scripts

--- wrf_run/post_processing/draw_meteograms.sh
+++ wrf_run/post_processing/draw_meteograms.sh
@@ -2,51 +2,52 @@
 # @Author: Benjamin Held
 # @Date:   2017-07-03 18:01:23
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-04-15 17:39:44
+# @Last Modified time: 2018-08-07 19:02:24
 
 function generate_meteogram () {
   METEO_TITLE=${1}
   LOC_SHORTCUT=${2}
   INPUT_FILE=${2}.d01.TS
 
-  # example ncl call which prints a meteogram for the given file and input data
-  #ncl time_array=${LEGEND_ARRAY} ticks=${TICK_ARRAY} sticks=${STICK_ARRAY} title=\"${METEO_TITLE}\" input=\"${INPUT_FILE}\" plot_meteogram  >> ${SCRIPT_PATH}/debug.log
+  # move required meteogram files
+  mv ${HOME}/Build_WRF/WRFV3/test/em_real/${LOC_SHORTCUT}.* ${HOME}/wrf_output
 
-  #move output to destination
+  ncl time_array=${LEGEND_ARRAY} ticks=${TICK_ARRAY} sticks=${STICK_ARRAY} title=\"${METEO_TITLE}\" input=\"${INPUT_FILE}\" plot_T_timeline  >> ${LOG_PATH}/debug.log
+  ncl time_array=${LEGEND_ARRAY} ticks=${TICK_ARRAY} sticks=${STICK_ARRAY} title=\"${METEO_TITLE}\" input=\"${INPUT_FILE}\" plot_meteogram  >> ${LOG_PATH}/debug.log
+
+  # mkdir ${DEST_FOLDER}/
+  # mv time_T2.png ${DEST_FOLDER}/${DEST_PREFIX}_time_T2_${LOC_SHORTCUT}.png
+  # mv meteo.png ${DEST_FOLDER}/${DEST_PREFIX}_meteogram_${LOC_SHORTCUT}.png
+
+  # optimize png size
+  find . -maxdepth 1 -name '*.png' -exec optipng {} \;
+
+  # move files to output folder
+  mv time_T2.png ${DEST_FOLDER}/${DEST_PREFIX}_time_T2_${LOC_SHORTCUT}.png
+  mv meteo.png ${DEST_FOLDER}/${DEST_PREFIX}_meteogram_${LOC_SHORTCUT}.png
 }
-
-set -e
-source ${HOME}/scripts/set_env.sh
 
 # logging time stamp
 SCRIPT_PATH=$(pwd)
 now=$(date +"%T")
-printf "Starting meteograms at ${now}.\n" >> ${SCRIPT_PATH}/log.info
+printf "Starting meteograms at ${now}.\n" >> ${LOG_PATH}/log.info
 
-cd ${HOME}/<output_folder>
+cd ${HOME}/wrf_output
 
-# optional addition to the storage path
-DEST_SUFFIX=${5}
-DATE=${1}-${2}-${3}
+DATE=$1-$2-$3
 START_DATE=$(LC_ALL=en_UTF-8 date +\(%Y-%m-%d-%HUTC\) -d "${DATE}T${4}:00")
 DATE_FORMAT="+%b-%d/00"
-# create an array with date stamps to show at the time axis
 LEGEND_ARRAY="(/\"$(LC_ALL=en_UTF-8 date $DATE_FORMAT -d "$DATE + 1 day")\""
 
-# create array for major tick mark positions at the beginning of a day
-# add the first position based on the starting hour of the model run
-MAIN_HOURS=`expr 24 - ${4}`
+MAIN_HOURS=`expr 24 - $4`
 TICK_ARRAY="(/${MAIN_HOURS}"
 
-# create arry for minor tick mark positions at midday
-# add the first position based on the starting hour of the model run
-SEC_HOURS=`expr 12 - ${4}`
+SEC_HOURS=`expr 12 - $4`
 STICK_ARRAY="(/${SEC_HOURS}"
 
-# add the tick mark positions and time stamps for the following days
 for i in {2..8}
 do
-  NEXT_DATE=$(LC_ALL=en_UTF-8 date $DATE_FORMAT -d "$DATE + ${i} day")
+  NEXT_DATE=$(LC_ALL=en_UTF-8 date $DATE_FORMAT -d "$DATE + $i day")
   SEC_HOURS=`expr $MAIN_HOURS + 12`
   STICK_ARRAY+=","
   STICK_ARRAY+="${SEC_HOURS}"
@@ -57,18 +58,17 @@ do
   TICK_ARRAY+="${MAIN_HOURS}"
 done
 
-# conclude the array
 LEGEND_ARRAY+="/)"
 TICK_ARRAY+="/)"
 STICK_ARRAY+="/)"
 
-# when putting the output into time stamp based folder, this creates a folder
-# prefix with mm_dd_hh/mm_dd_hh
-DEST_PREFIX=${2}_${3}_${4}/${2}_${3}_${4}${DEST_SUFFIX}
+DEST_PREFIX=${2}_${3}_${4}
+DEST_FOLDER=${6}
 
-# function call to draw the output for one specific place
-generate_meteogram "Hannover,_Germany_${START_DATE}" "Han"
+generate_meteogram "Hannover,_NDS_${START_DATE}" "Han"
+generate_meteogram "Ith-Zeltplatz,_NDS_${START_DATE}" "Ith"
+generate_meteogram "Husum,_SH_${START_DATE}" "Hus"
 
 # logging time stamp
 now=$(date +"%T")
-printf "Finished meteograms at ${now}.\n" >> ${SCRIPT_PATH}/log.info
+printf "Finished meteograms at ${now}.\n" >> ${LOG_PATH}/log.info
--- wrf_run/post_processing/draw_plots.sh
+++ wrf_run/post_processing/draw_plots.sh
@@ -2,14 +2,12 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-12 16:04:54
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-04-14 19:32:58
-
-source ${HOME}/scripts/set_env.sh
+# @Last Modified time: 2018-08-07 19:01:17
 
 # logging time stamp
 SCRIPT_PATH=$(pwd)
 now=$(date +"%T")
-printf "Starting output generation at ${now}.\n" >> ${SCRIPT_PATH}/log.info
+printf "Starting output generation at ${now}.\n" >> ${LOG_PATH}/log.info
 
 YEAR=${1}
 MONTH=${2}
@@ -18,33 +16,38 @@ HOUR=${4}
 PERIOD=${5}
 # optional addition to the storage path
 DEST_SUFFIX='_test'
+DEST_FOLDER="${SCRIPT_PATH}/${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}"
 
 # create parent folder for time stamp
-mkdir ./${MONTH}_${DAY}_${HOUR}
+mkdir ${DEST_FOLDER}
 
 # generate all listed meteograms
 cd ${SCRIPT_PATH}
-sh ./draw_meteograms.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD} ${DEST_SUFFIX}
+sh ./draw_meteograms.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD} ${DEST_FOLDER}
 
 cd ${HOME}/wrf_output
 
 # generate output
-ncl plot_timestamp_output >> ${SCRIPT_PATH}/debug.log
-ncl plot_tot_rain >> ${SCRIPT_PATH}/debug.log
+ncl plot_timestamp_output >> ${LOG_PATH}/debug.log
+ncl plot_tot_rain >> ${LOG_PATH}/debug.log
 
 find . -maxdepth 1 -name '*.png' -exec optipng {} \;
 
 # create folder and move output
-mkdir ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/comp
-mkdir ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/rain_3h
-mkdir ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/rain_tot
-mkdir ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/thunderstorm_index
+mkdir ${DEST_FOLDER}/comp
+mkdir ${DEST_FOLDER}/rain_3h
+mkdir ${DEST_FOLDER}/rain_tot
+mkdir ${DEST_FOLDER}/thunderstorm_index
 
-mv comp_*.png ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/comp
-mv rain_3h_*.png ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/rain_3h
-mv rain_tot_*.png ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/rain_tot
-mv thunderstorm_*.png ./${MONTH}_${DAY}_${HOUR}${DEST_SUFFIX}/thunderstorm_index
+mv comp_*.png ${DEST_FOLDER}/comp
+mv rain_3h_*.png ${DEST_FOLDER}/rain_3h
+mv rain_tot_*.png ${DEST_FOLDER}/rain_tot
+mv thunderstorm_*.png ${DEST_FOLDER}/thunderstorm_index
+
+# generate meat.ini file
+cd ${SCRIPT_PATH}
+sh create_ini.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD} ${DEST_FOLDER}
 
 # logging time stamp
 now=$(date +"%T")
-printf "Finished output generation at ${now}.\n" >> ${SCRIPT_PATH}/log.info
+printf "Finished output generation at ${now}.\n" >> ${LOG_PATH}/log.info
