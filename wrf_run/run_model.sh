a26e8e55aac91b086b387bb408dc533efcb881be
Benjamin Held <me@bheld.eu>
2018-08-10 15:01:45 +0200

    added locking to main script

--- wrf_run/run_model.sh
+++ wrf_run/run_model.sh
@@ -2,12 +2,14 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-18 09:40:15
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-08-03 17:00:32
+# @Last Modified time: 2018-08-10 14:58:32
 
 # main script for starting a wrf model run
 # Version 0.1.0
 # created by Benjamin Held and other sources, June 2017
-# $1: the starting hour of the model run
+# Two possible parameter sets:
+# <START_HOUR> <PERIOD> <RESOLUTION> <PERIOD>
+# <START_YEAR> <START_MONTH> <START_DAY> <START_HOUR> <PERIOD> <RESOLUTION>
 
 function error_exit () {
   now=$(date +"%T")
@@ -17,9 +19,6 @@ function error_exit () {
   cd ${SCRIPT_PATH}
   sh create_mail.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} "${ERROR_STATUS}" "Fail"
   echo "${1}" 1>&2
-  if [ "${RESOLUTION}" != "1p00" ]; then
-    sh run_model.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD} "1p00"
-  fi
   exit 1
 }
 
@@ -49,17 +48,22 @@ elif [ "$#" -eq 6 ]; then
   RESOLUTION=${6}
 else
   echo "Wrong number of arguments."
-  echo "Must either be two for <HOUR> <PERIOD> or five for <YEAR> <MONTH> <DAY> <HOUR> <PERIOD>"
+  echo "Must either be three for <HOUR> <PERIOD> <RESOLUTION>"
+  echo "or six for <YEAR> <MONTH> <DAY> <HOUR> <PERIOD> <RESOLUTION>"
   exit 1
 fi
 
+LCK="./lock.file";
+exec 42>${LCK};
+
+flock -x 42;
 # preparing status file
 printf "Starting new model run for: ${YEAR}/${MONTH}/${DAY} ${HOUR}:00 UTC.\n" > ${STATUS_FILE}
 
 # adjusting namelist for next run
 cd ${SCRIPT_PATH}/model_run
 printf "Starting namelist preparation.\n" >> ${STATUS_FILE}
-sh prepare_namelist.sh ${BUILD_PATH} ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}; ret=${?}
+sh prepare_namelist.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}; ret=${?}
 if ! [ ${ret} == 0 ]; then
   error_exit "Failed to prepare the namelist files"
 fi
@@ -75,7 +79,7 @@ fi
 # start model run
 cd ${SCRIPT_PATH}/model_run
 printf "Starting model run and preparation.\n" >> ${STATUS_FILE}
-sh run_preprocessing.sh ${BUILD_PATH} ${GFS_PATH} ${RESOLUTION}; ret=${?}
+sh run_preprocessing.sh ${GFS_PATH} ${RESOLUTION}; ret=${?}
 cd ${SCRIPT_PATH}
 if ! [ ${ret} == 0 ]; then
     cd ${BUILD_PATH}/WRFV3/test/em_real/
@@ -95,8 +99,8 @@ sh draw_plots.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}; ret=${?}
 if [ ${ret} == 0 ]; then
   printf "Starting archive generation.\n" >> ${STATUS_FILE}
   cd ${HOME}/wrf_output
-  tar czf wrfout_${YEAR}_${MONTH}_${DAY}_${HOUR}.tar.gz wrfout_d01_* Han.d01.* Ith.d01.*
-  mv wrfout_${YEAR}_${MONTH}_${DAY}_${HOUR}.tar.gz history/
+  #tar czf wrfout_${YEAR}_${MONTH}_${DAY}_${HOUR}.tar.gz wrfout_d01_* Han.d01.* Ith.d01.*
+  #mv wrfout_${YEAR}_${MONTH}_${DAY}_${HOUR}.tar.gz history/
 else
     error_exit "Error while creating output files"
 fi
