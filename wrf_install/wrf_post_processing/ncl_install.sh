5aca57ed0bf5f866fe30b2a03e4dbccf12f34549
Benjamin Held <me@bheld.eu>
2018-08-27 20:10:15 +0200

    adjusted ncl install script

--- wrf_install/wrf_post_processing/ncl_install.sh
+++ wrf_install/wrf_post_processing/ncl_install.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-26 17:34:20
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2017-12-27 15:17:32
+# @Last Modified time: 2018-08-27 20:09:35
 
 # Script to compile the ncar command language for output visualization
 # $1: the path to the folder where the ncl program should be installed
@@ -14,24 +14,25 @@ LIGHT_BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+NCL_NAME='ncl_ncarg-6.4.0-Debian8.6_64bit_nodap_gnu492.tar.gz'
+
 # Script to extract the optional ncl package for postprocessing
 # Path to the library folder
-cd ${HOME}/${1}
+cd ${DIR}
 
 # Unpacking ncl files
 printf "${YELLOW}\nUnpacking ncl.tar files: ${NC}\n"
 mkdir ncl
-mv ncl_ncarg-6.4.0-Debian8.6_64bit_nodap_gnu492.tar.gz
-cd ncl
-tar ncl_ncarg-6.4.0-Debian8.6_64bit_nodap_gnu492.tar.gz
+wget https://www.earthsystemgrid.org/dataset/ncl.640.nodap/file/${NCL_NAME}
+tar -xzf ${NCL_NAME}
 
 # cleanup tar
-rm ncl_ncarg-6.4.0-Debian8.6_64bit_nodap_gnu492.tar.gz
+rm ${NCL_NAME}
 
 # Installing additional library
 printf "${YELLOW}\nInstaling additional libraries: ${NC}\n"
-yaourt -S fontconfig
+yaourt -S --needed fontconfig libxrender libxtst
 # Installing optipng to optimize png output size
-yaourt -S optipng
+yaourt -S --needed optipng
 
 printf "${LIGHT_BLUE}\nFinished ncl preparations. ${NC}\n"
