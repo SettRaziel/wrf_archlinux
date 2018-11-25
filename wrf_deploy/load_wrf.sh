42259d286b3951def219f85581dbc19aaa9222f1
Benjamin Held <me@bheld.eu>
2018-11-25 20:19:29 +0100

    added wrf_390 for deployment

--- wrf_deploy/load_wrf.sh
+++ wrf_deploy/load_wrf.sh
@@ -2,12 +2,13 @@
 # @Author: Benjamin Held
 # @Date:   2018-10-23 09:09:29
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-11-18 10:37:51
+# @Last Modified time: 2018-11-25 20:18:30
 
 function print_options () {
   printf "${YELLOW} 1: WRFV3 version 3.9.1\n${NC}"
-  printf "${YELLOW} 2: WRFV3 version 3.8.1\n${NC}"
-  printf "${YELLOW} 3: WRFV3 version 3.8.0\n${NC}"
+  printf "${YELLOW} 2: WRFV3 version 3.9.0\n${NC}"
+  printf "${YELLOW} 3: WRFV3 version 3.8.1\n${NC}"
+  printf "${YELLOW} 4: WRFV3 version 3.8.0\n${NC}"
 }
 
 # define terminal colors
@@ -28,22 +29,23 @@ then
     print_options        
     read INPUT
     case ${INPUT} in
-      [123]* ) SELECT_VALUE=${INPUT}; break;;
-      * ) printf "${RED}Please use a numeric value in [1-3].${NC}\n";;
+      [1234]* ) SELECT_VALUE=${INPUT}; break;;
+      * ) printf "${RED}Please use a numeric value in [1-4].${NC}\n";;
     esac
   done
 else
   case ${1} in
-    [123]* ) SELECT_VALUE=${1} ;;
+    [1234]* ) SELECT_VALUE=${1} ;;
     ['--help']* ) printf "${LIGHT_BLUE}Usage:\n${NC}"; print_options;;
-    * ) printf "${RED}Error: False argument. Please use a numeric value in [1-3] or --help.${NC}\n";;
+    * ) printf "${RED}Error: False argument. Please use a numeric value in [1-4] or --help.${NC}\n";;
   esac
 fi
 
 case ${SELECT_VALUE} in
   [1]* ) FILE_NAME='wrf_dev_391.tar.gz';;
-  [2]* ) FILE_NAME='wrf_dev_381.tar.gz';;
-  [3]* ) FILE_NAME='wrf_dev_380.tar.gz';;
+  [2]* ) FILE_NAME='wrf_dev_390.tar.gz';;
+  [3]* ) FILE_NAME='wrf_dev_381.tar.gz';;
+  [4]* ) FILE_NAME='wrf_dev_380.tar.gz';;
 esac
 
 # creating url for the selectied wrf tar
