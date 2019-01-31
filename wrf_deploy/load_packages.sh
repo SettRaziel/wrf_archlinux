2dfecdafede6f29c3f4dc3f11d9e09adfcdf23bb
Benjamin Held <me@bheld.eu>
2019-01-31 18:04:02 +0100

    changed deployment to yay

--- wrf_deploy/load_packages.sh
+++ wrf_deploy/load_packages.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2018-09-07 16:35:49
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-11-24 00:08:50
+# @Last Modified time: 2019-01-31 18:03:28
 
 # define terminal colors
 RED='\033[0;31m'
@@ -13,10 +13,10 @@ NC='\033[0m' # No Color
 
 # Installing packages for running the model
 printf "${YELLOW}\nInstalling required libraries: ${NC}\n"
-yaourt -S --needed tcsh wget curl
+yay -S --needed tcsh wget curl
 
 # Installing required packages for running ncl
 printf "${YELLOW}\nInstalling additional ncl libraries: ${NC}\n"
-yaourt -S --needed fontconfig libxrender libxtst
+yay -S --needed fontconfig libxrender libxtst
 # Installing optipng to optimize png output size
-yaourt -S --needed optipng
+yay -S --needed optipng
