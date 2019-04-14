17a540a5d48f5d2ebac8c0d95559066e50425e72
Benjamin Held <me@bheld.eu>
2019-04-14 12:16:00 +0200

    added msmtp package to wrf_deploy

--- wrf_deploy/load_packages.sh
+++ wrf_deploy/load_packages.sh
@@ -2,13 +2,13 @@
 # @Author: Benjamin Held
 # @Date:   2018-09-07 16:35:49
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-02-22 17:58:05
+# @Last Modified time: 2019-04-14 12:14:28
 
 # define terminal colors
 source ../libs/terminal_color.sh
 
 # Installing packages for running the model
-printf "${YELLOW}\nInstalling required libraries: ${NC}\n"
+printf "${YELLOW}\nInstalling required model libraries: ${NC}\n"
 yay -S --needed tcsh wget curl
 
 # Installing required packages for running ncl
@@ -16,3 +16,7 @@ printf "${YELLOW}\nInstalling additional ncl libraries: ${NC}\n"
 yay -S --needed fontconfig libxrender libxtst
 # Installing optipng to optimize png output size
 yay -S --needed optipng
+
+# Installing packages to send emails
+printf "${YELLOW}\nInstalling required mail libraries: ${NC}\n"
+yay -S --needed msmtp
