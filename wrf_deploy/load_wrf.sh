969f6750e2e1ae548a66b0793f9819e29b397946
Benjamin Held <me@bheld.eu>
2019-06-26 21:03:03 +0200

    add wrf_400 to loading script

--- wrf_deploy/load_wrf.sh
+++ wrf_deploy/load_wrf.sh
@@ -2,20 +2,22 @@
 # @Author: Benjamin Held
 # @Date:   2018-10-23 09:09:29
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-02-22 17:49:30
+# @Last Modified time: 2019-06-26 21:02:44
 
+# help output
 function print_options () {
-  printf "${YELLOW} 1: WRFV3 version 3.9.1\n${NC}"
-  printf "${YELLOW} 2: WRFV3 version 3.9.0\n${NC}"
-  printf "${YELLOW} 3: WRFV3 version 3.8.1\n${NC}"
-  printf "${YELLOW} 4: WRFV3 version 3.8.0\n${NC}"
+  printf "${YELLOW} 1: WRFV4 version 4.0.2\n${NC}"
+  printf "${YELLOW} 2: WRFV3 version 3.9.1\n${NC}"
+  printf "${YELLOW} 3: WRFV3 version 3.9.0\n${NC}"
+  printf "${YELLOW} 4: WRFV3 version 3.8.1\n${NC}"
+  printf "${YELLOW} 5: WRFV3 version 3.8.0\n${NC}"
 }
 
 # define terminal colors
 source ../libs/terminal_color.sh
 
 # Default selection
-SELECT_VALUE=1
+SELECT_VALUE=2
 
 # check for script arguments
 if [ $# -ne 1 ] 
@@ -25,23 +27,24 @@ then
     print_options        
     read INPUT
     case ${INPUT} in
-      [1234]* ) SELECT_VALUE=${INPUT}; break;;
-      * ) printf "${RED}Please use a numeric value in [1-4].${NC}\n";;
+      [12345]* ) SELECT_VALUE=${INPUT}; break;;
+      * ) printf "${RED}Please use a numeric value in [1-5].${NC}\n";;
     esac
   done
 else
   case ${1} in
-    [1234]* ) SELECT_VALUE=${1} ;;
+    [12345]* ) SELECT_VALUE=${1} ;;
     ['--help']* ) printf "${LIGHT_BLUE}Usage:\n${NC}"; print_options;;
-    * ) printf "${RED}Error: False argument. Please use a numeric value in [1-4] or --help.${NC}\n";;
+    * ) printf "${RED}Error: False argument. Please use a numeric value in [1-5] or --help.${NC}\n";;
   esac
 fi
 
 case ${SELECT_VALUE} in
-  [1]* ) FILE_NAME='wrf_391';;
-  [2]* ) FILE_NAME='wrf_390';;
-  [3]* ) FILE_NAME='wrf_381';;
-  [4]* ) FILE_NAME='wrf_380';;
+  [1]* ) FILE_NAME='wrf_400';;
+  [2]* ) FILE_NAME='wrf_391';;
+  [3]* ) FILE_NAME='wrf_390';;
+  [4]* ) FILE_NAME='wrf_381';;
+  [5]* ) FILE_NAME='wrf_380';;
 esac
 
 # creating url for the selectied wrf tar
