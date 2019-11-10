95a41010a212286e4aff18ca7c7508fbf0f17a8d
Benjamin Held <me@bheld.eu>
2019-11-10 19:13:11 +0100

    refactoring: quoted variables to prevent word splitting in wrf_run

--- wrf_run/run_model.sh
+++ wrf_run/run_model.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-18 09:40:15
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-11-04 17:02:04
+# @Last Modified time: 2019-11-10 19:11:59
 
 # main script for starting a wrf model run
 # Version 0.4.0
@@ -14,10 +14,10 @@
 function error_exit () {
   NOW=$(date +"%T")
   ERROR_STATUS="${1} at: ${NOW}."
-  printf "${ERROR_STATUS}\\n" >> ${ERROR_LOG}
-  printf "Error: ${1} at: ${NOW}.\\n" >> ${STATUS_FILE}
-  cd ${SCRIPT_PATH}
-  sh create_mail.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} "${ERROR_STATUS}" "Fail"
+  printf "${ERROR_STATUS}\\n" >> "${ERROR_LOG}"
+  printf "Error: ${1} at: ${NOW}.\\n" >> "${STATUS_FILE}"
+  cd "${SCRIPT_PATH}"
+  sh create_mail.sh "${YEAR}" "${MONTH}" "${DAY}" "${HOUR}" "${ERROR_STATUS}" "Fail"
   echo "${1}" 1>&2
   exit 1
 }
@@ -25,7 +25,7 @@ function error_exit () {
 # imports
 SCRIPT_PATH=$(pwd)
 BUILD_PATH="<wrf path>"
-source ${SCRIPT_PATH}/set_env.sh ${BUILD_PATH}
+source "${SCRIPT_PATH}/set_env.sh" "${BUILD_PATH}"
 export LOG_PATH=${SCRIPT_PATH}/logs
 
 # default variables
@@ -59,53 +59,53 @@ exec 42>${LCK};
 
 flock -x 42;
 # preparing status file
-printf "Starting new model run for: ${YEAR}/${MONTH}/${DAY} ${HOUR}:00 UTC.\\n" > ${STATUS_FILE}
+printf "Starting new model run for: ${YEAR}/${MONTH}/${DAY} ${HOUR}:00 UTC.\\n" > "${STATUS_FILE}"
 
 # adjusting namelist for next run
-cd ${SCRIPT_PATH}/model_run
-printf "Starting namelist preparation.\\n" >> ${STATUS_FILE}
-sh prepare_namelist.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}; RET=${?}
+cd "${SCRIPT_PATH}/model_run"
+printf "Starting namelist preparation.\\n" >> "${STATUS_FILE}"
+sh prepare_namelist.sh "${YEAR}" "${MONTH}" "${DAY}" "${HOUR}" "${PERIOD}"; RET=${?}
 if ! [ ${RET} == 0 ]; then
   error_exit "Failed to prepare the namelist files"
 fi
 
 # fetching input data
-cd ${SCRIPT_PATH}/data_fetch
-printf "Starting namelist preparation.\\n" >> ${STATUS_FILE}
-sh gfs_fetch.sh "${YEAR}${MONTH}${DAY}" ${HOUR} ${GFS_PATH} ${RESOLUTION} ${PERIOD}; RET=${?}
+cd "${SCRIPT_PATH}/data_fetch"
+printf "Starting namelist preparation.\\n" >> "${STATUS_FILE}"
+sh gfs_fetch.sh "${YEAR}${MONTH}${DAY}" "${HOUR}" "${GFS_PATH}" "${RESOLUTION}" "${PERIOD}"; RET=${?}
 if ! [ ${RET} == 0 ]; then
   error_exit "Failed to prepare the namelist files"
 fi
 
 # start model run
-cd ${SCRIPT_PATH}/model_run
-printf "Starting model run and preparation.\\n" >> ${STATUS_FILE}
-sh run_preprocessing.sh ${GFS_PATH} ${RESOLUTION}; RET=${?}
-cd ${SCRIPT_PATH}
+cd "${SCRIPT_PATH}/model_run"
+printf "Starting model run and preparation.\\n" >> "${STATUS_FILE}"
+sh run_preprocessing.sh "${GFS_PATH}" "${RESOLUTION}"; RET=${?}
+cd "${SCRIPT_PATH}"
 if ! [ ${RET} == 0 ]; then
-    cd ${BUILD_PATH}/WRF/test/em_real/
+    cd "${BUILD_PATH}/WRF/test/em_real/"
     rm wrfout_d01_*
     error_exit "Failed to run the model"
 fi
 
 # move output files
-cd ${SCRIPT_PATH}/model_run
+cd "${SCRIPT_PATH}/model_run"
 sh clean_up_output.sh
-mv ${BUILD_PATH}/WRF/test/em_real/wrfout_d01_* ${HOME}/wrf_output
+mv "${BUILD_PATH}/WRF/test/em_real/wrfout_d01_*" "${HOME}/wrf_output"
 
 # run output script
-cd ${SCRIPT_PATH}/post_processing
-printf "Starting postprocessing.\\n" >> ${STATUS_FILE}
-sh draw_plots.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} ${PERIOD}; RET=${?}
+cd "${SCRIPT_PATH}/post_processing"
+printf "Starting postprocessing.\\n" >> "${STATUS_FILE}"
+sh draw_plots.sh "${YEAR}" "${MONTH}" "${DAY}" "${HOUR}" "${PERIOD}"; RET=${?}
 if [ ${RET} == 0 ]; then
-  printf "Starting archive generation.\\n" >> ${STATUS_FILE}
-  cd ${HOME}/wrf_output
+  printf "Starting archive generation.\\n" >> "${STATUS_FILE}"
+  cd "${HOME}/wrf_output"
   #tar czf wrfout_${YEAR}_${MONTH}_${DAY}_${HOUR}.tar.gz wrfout_d01_* Han.d01.* Ith.d01.*
   #mv wrfout_${YEAR}_${MONTH}_${DAY}_${HOUR}.tar.gz history/
 else
     error_exit "Error while creating output files"
 fi
 
-cd ${SCRIPT_PATH}
-sh create_mail.sh ${YEAR} ${MONTH} ${DAY} ${HOUR} "Finished model run without error." "Success"
-printf "Finished model run without error.\\n" >> ${STATUS_FILE}
+cd "${SCRIPT_PATH}"
+sh create_mail.sh "${YEAR}" "${MONTH}" "${DAY}" "${HOUR}" "Finished model run without error." "Success"
+printf "Finished model run without error.\\n" >> "${STATUS_FILE}"
