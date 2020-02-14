218fd0dd5acc6b2752a79d28667657d88f232f2e
Benjamin Held <me@bheld.eu>
2020-02-14 15:38:41 +0100

    removed unused variable and added files for cleanup

--- wrf_run/model_run/clean_up_output.sh
+++ wrf_run/model_run/clean_up_output.sh
@@ -2,15 +2,13 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-12 09:26:31
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-02-08 12:59:19
+# @Last Modified time: 2020-02-14 15:38:01
 
 # Script to run the preprocessing operations
 
 # setting -e to abort on error
 set -e
 
-# cleaning up in wps preprocessing folder
-SCRIPT_PATH=$(pwd)
 now=$(date +"%T")
 if [ -z "${LOG_PATH}" ]; then
   printf "Cleaning up output data from last time at %s\\n" "${now}"
@@ -19,6 +17,7 @@ else
 fi
 
 # remove previous output files
+rm "${BUILD_PATH}"/WRF/test/em_real/wrfrst_*
 rm "${HOME}"/wrf_output/wrfout_*
 rm "${HOME}"/wrf_output/*.PH
 rm "${HOME}"/wrf_output/*.QV
