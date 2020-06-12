2b732b7a26687dd42bd031bf28cb83f8fb5ee76d
Benjamin Held <me@bheld.eu>
2020-06-12 20:23:11 +0200

    removed ncl from model install

--- wrf_install/wrf_postprocessing/wrf_postprocessing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# @Author: Benjamin Held
-# @Date:   2018-09-03 09:47:13
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-04-30 17:17:14
-
-# install ncl as postprocessing option
-sh ncl_install.sh
