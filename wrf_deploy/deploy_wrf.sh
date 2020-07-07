13d6c78dade8478d91006a0a2f5f960b246fd08c
Benjamin Held <me@bheld.eu>
2020-07-07 21:46:09 +0200

    added condition for gfs directory creation

--- wrf_deploy/deploy_wrf.sh
+++ wrf_deploy/deploy_wrf.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2018-11-15 18:08:23
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-07-03 18:38:57
+# @Last Modified time: 2020-07-07 21:45:28
 
 # main script to deploy a pre compiled version of wrf
 # Version 0.4.6
@@ -27,7 +27,9 @@ fi
 sh ./load_packages.sh
 
 # create storage folder for the gfs input data
-mkdir ${HOME}/gfs_data
+if ! [ -d "${HOME}/gfs_data" ]; then
+  mkdir "${HOME}/gfs_data"
+fi
 
 # load and unpack the neccessary geodata, WRFV4 minimal
 # using source to get the environment variable for WRF_GEODATA_INDEX
