65af2b7b2ab65b03e8b8b4e0e36801c197cab82a
Benjamin Held <me@bheld.eu>
2020-07-27 18:26:26 +0200

    adjusted library version for faster compile time

--- wrf_install/linux/set_env.sh
+++ wrf_install/linux/set_env.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2017-03-03 17:20:53
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-07-25 20:21:09
+# @Last Modified time: 2020-07-27 18:25:41
 
 # Script that sets the required variables for the model installation
 # ${1}: the build path where the wrf model should be installed
@@ -14,7 +14,7 @@ export CXX="g++"
 export FC="gfortran"
 export FCFLAGS="-m64"
 export F77="gfortran"
-export FFLAGS="-m64 -fallow-argument-mismatch"
+export FFLAGS="-m64"
 export PATH="${PATH}:${DIR}/netcdf/bin"
 export NETCDF="${DIR}/netcdf"
 export HDF5="${DIR}/hdf5"
@@ -28,13 +28,13 @@ export JASPERINC="${DIR}/grib2/include"
 # optional: required when using ncl for output in the library folder
 export NCARG_ROOT="${DIR}/ncl"
 
-# setting library versions for usage (last checked: 2020-04-04)
+# setting library versions for usage (last checked: 2020-07-26)
 export WRF_VERSION="4.2"              # latest: 4.2
-export WPS_VERSION="4.1"              # latest: 4.2
+export WPS_VERSION="4.2"              # latest: 4.2
 export HDF_VERSION="1.12"             # latest: 1.12.0
 export NETCDF_VERSION="4.7.4"         # latest: 4.7.4
 export NETCDF_FORTRAN_VERSION="4.5.3" # latest: 4.5.3
-export MPI_VERSION="3.4a3"            # latest: 3.4a3
+export MPI_VERSION="3.3"              # latest: 3.4a3
 export LIBPNG_VERSION="1.6.37"        # latest: 1.6.37
 export ZLIB_VERSION="1.2.11"          # latest: 1.2.11
 export JASPER_VERSION="1.900.2"       # latest: 1.900.29 / 2.0.14
