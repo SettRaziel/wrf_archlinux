4aa2cef94d90763645b84d88156254708a7d51bd
Benjamin Held <me@bheld.eu>
2021-12-31 22:40:56 +0100

    drop WRFV3 from load script

--- wrf_deploy/load_wrf.sh
+++ wrf_deploy/load_wrf.sh
@@ -6,7 +6,6 @@
 # 1: WRFV4 version 4.2
 # 2: WRFV4 version 4.1.5
 # 3: WRFV4 version 4.0.2
-# 4: WRFV3 version 3.9.1
 
 # enable termination on error
 set -e
@@ -16,7 +15,6 @@ print_options () {
   printf "%b 1: WRFV4 version 4.2\\n%b" "${YELLOW}" "${NC}"
   printf "%b 2: WRFV4 version 4.1.5\\n%b" "${YELLOW}" "${NC}"
   printf "%b 3: WRFV4 version 4.0.2\\n%b" "${YELLOW}" "${NC}"
-  printf "%b 4: WRFV3 version 3.9.1\\n%b" "${YELLOW}" "${NC}"
 }
 
 # downloading and unpacking archive
@@ -39,15 +37,15 @@ if [ -z "${WRF_VERSION_INDEX}" ]; then
     print_options        
     read -r INPUT
     case ${INPUT} in
-      [1234]* ) WRF_VERSION_INDEX=${INPUT}; break;;
-      * ) printf "%bPlease use a numeric value in [1-4].%b\\n" "${RED}" "${NC}";;
+      [123]* ) WRF_VERSION_INDEX=${INPUT}; break;;
+      * ) printf "%bPlease use a numeric value in [1-3].%b\\n" "${RED}" "${NC}";;
     esac
   done
 else
   case ${WRF_VERSION_INDEX} in
-    [1234]* ) ;;
+    [123]* ) ;;
     ['--help']* ) printf "%bUsage:\\n%b" "${LIGHT_BLUE}" "${NC}"; print_options;;
-    * ) printf "%bError: False argument. Please use a numeric value in [1-4] or --help.%b\\n" "${RED}" "${NC}"; exit 1;;
+    * ) printf "%bError: False argument. Please use a numeric value in [1-3] or --help.%b\\n" "${RED}" "${NC}"; exit 1;;
   esac
 fi
 
@@ -55,7 +53,6 @@ case ${WRF_VERSION_INDEX} in
   [1]* ) FILE_NAME='wrf_420'; WRF_FOLDER='WRF-4.2'; WPS_FOLDER='WPS-4.2';;
   [2]* ) FILE_NAME='wrf_410'; WRF_FOLDER='WRF-4.1.5'; WPS_FOLDER='WPS-4.1';;
   [3]* ) FILE_NAME='wrf_400'; WRF_FOLDER='WRF'; WPS_FOLDER='WPS';;
-  [4]* ) FILE_NAME='wrf_391'; WRF_FOLDER='WRFV3'; WPS_FOLDER='WPS';;
 esac
 
 # creating url for the selectied wrf tar
@@ -81,10 +78,7 @@ fi
 # copying the config files from the repository to its destination
 printf "%b\\nDeploying repository config files: %b\\n" "${YELLOW}" "${NC}"
 cd "${SCRIPT_PATH}" || exit 1
-case ${WRF_GEODATA_INDEX} in
-    [4]* ) cp ../additions/config/namelist_low_res.wps "${HOME}/${FILE_NAME}/${WPS_FOLDER}/namelist.wps";;
-    * ) cp ../additions/config/namelist.wps "${HOME}/${FILE_NAME}/${WPS_FOLDER}";;
-esac
+cp ../additions/config/namelist.wps "${HOME}/${FILE_NAME}/${WPS_FOLDER}";;
 cp ../additions/config/namelist.input "${HOME}/${FILE_NAME}/${WRF_FOLDER}/test/em_real/"
 cp ../additions/config/tslist "${HOME}/${FILE_NAME}/${WRF_FOLDER}/test/em_real/"
 
