f02da3f8725a3db9b6d0f1f250964281eed5490c
Benjamin Held <me@bheld.eu>
2022-04-17 20:02:19 +0200

    bump to v0.7.0

--- wrf_deploy/deploy_wrf.sh
+++ wrf_deploy/deploy_wrf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # main script to deploy a pre compiled version of wrf
-# Version 0.6.1
+# Version 0.7.0
 # created by Benjamin Held and other sources, June 2017
 # possible parameter:
 # --default
