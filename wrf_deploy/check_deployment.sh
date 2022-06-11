fab4ba812731eade074ac937eae9d47dce856938
Benjamin Held <me@bheld.eu>
2022-06-11 22:51:07 +0200

    add check for gfs data directory

--- wrf_deploy/check_deployment.sh
+++ wrf_deploy/check_deployment.sh
@@ -13,3 +13,10 @@ if ! [ -d "${HOME}/geo_data" ]; then
   printf "%bDirectory for geodata cannot be found.%b\\n" "${RED}" "${NC}";;
   exit 1
 fi
+
+# checking if gfs data directory is present
+printf "%bChecking for gfs input data folder...\\n%b" "${YELLOW}" "${NC}"        
+if ! [ -d "${HOME}/gfs_data" ]; then
+  printf "%bDirectory for gfs input data cannot be found.%b\\n" "${RED}" "${NC}";;
+  exit 1
+fi
