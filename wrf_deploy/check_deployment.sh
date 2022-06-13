293ddb82a532a44ab7790879b942a0e26c1064c0
Benjamin Held <me@bheld.eu>
2022-06-13 21:15:33 +0200

    add check for wrf directory

--- wrf_deploy/check_deployment.sh
+++ wrf_deploy/check_deployment.sh
@@ -4,6 +4,8 @@
 # enable termination on error
 set -e
 
+DEPLOY_DIR="${1}"
+
 # define terminal colors
 . ../libs/terminal_color.sh
 
@@ -18,5 +20,12 @@ fi
 printf "%bChecking for gfs input data folder...\\n%b" "${YELLOW}" "${NC}"        
 if ! [ -d "${HOME}/gfs_data" ]; then
   printf "%bDirectory for gfs input data cannot be found.%b\\n" "${RED}" "${NC}";;
+  exit 1
+fi
+
+# checking if wrf directory is present
+printf "%bChecking for wrf data folder...\\n%b" "${YELLOW}" "${NC}"        
+if ! [ -d "${HOME}/${DEPLOY_DIR}" ]; then
+  printf "%bDirectory for wrf data cannot be found.%b\\n" "${RED}" "${NC}";;
   exit 1
 fi
