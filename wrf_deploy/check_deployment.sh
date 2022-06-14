669f72e04cebe2e1d306d485a39c1c6fe054ed00
Benjamin Held <me@bheld.eu>
2022-06-14 19:10:01 +0200

    correct print statements

--- wrf_deploy/check_deployment.sh
+++ wrf_deploy/check_deployment.sh
@@ -12,20 +12,20 @@ DEPLOY_DIR="${1}"
 # checking if geodata directory is present
 printf "%bChecking for geodata folder...\\n%b" "${YELLOW}" "${NC}"        
 if ! [ -d "${HOME}/geo_data" ]; then
-  printf "%bDirectory for geodata cannot be found.%b\\n" "${RED}" "${NC}";;
+  printf "%bDirectory for geodata cannot be found.%b\\n" "${RED}" "${NC}";
   exit 1
 fi
 
 # checking if gfs data directory is present
 printf "%bChecking for gfs input data folder...\\n%b" "${YELLOW}" "${NC}"        
 if ! [ -d "${HOME}/gfs_data" ]; then
-  printf "%bDirectory for gfs input data cannot be found.%b\\n" "${RED}" "${NC}";;
+  printf "%bDirectory for gfs input data cannot be found.%b\\n" "${RED}" "${NC}";
   exit 1
 fi
 
 # checking if wrf directory is present
 printf "%bChecking for wrf data folder...\\n%b" "${YELLOW}" "${NC}"        
 if ! [ -d "${HOME}/${DEPLOY_DIR}" ]; then
-  printf "%bDirectory for wrf data cannot be found.%b\\n" "${RED}" "${NC}";;
+  printf "%bDirectory for wrf data cannot be found.%b\\n" "${RED}" "${NC}";
   exit 1
 fi
