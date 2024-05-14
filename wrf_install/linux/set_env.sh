db43d00e0ecdfde413de13ef282deb563e48570e
Benjamin Held <me@bheld.eu>
2024-05-14 06:31:23 +0200

    update software versions

--- wrf_install/linux/set_env.sh
+++ wrf_install/linux/set_env.sh
@@ -24,12 +24,12 @@ export WRFIO_NCD_LARGE_FILE_SUPPORT=1
 export NETCDF_classic=1
 
 # setting library versions for usage (last checked: 2024-05-12)
-export WRF_VERSION="4.5"              # latest: 4.6
+export WRF_VERSION="4.6"              # latest: 4.6
 export WPS_VERSION="4.5"              # latest: 4.5
-export NOAHMP_VERSION="5.0"           # latest: 5.0
-export HDF_VERSION="1_14_1"           # latest: 1.14.2
+export NOAHMP_VERSION="4.6"           # latest: 5.0
+export HDF_VERSION="1_14_2"           # latest: 1.14.2
 export NETCDF_VERSION="4.9.2"         # latest: 4.9.2
 export NETCDF_FORTRAN_VERSION="4.6.1" # latest: 4.6.1
-export MPI_VERSION="4.1.2"            # latest: 4.2.1
+export MPI_VERSION="4.2.1"            # latest: 4.2.1
 
 export WRF_DIR="${1}/WRF-${WRF_VERSION}"
