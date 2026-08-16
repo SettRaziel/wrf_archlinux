4806e31d532e0456d4e87114785d00713411a804
Benjamin Held <me@bheld.eu>
2026-08-16 10:23:41 +0200

    update wrf_deploy help text for default argument

--- wrf_deploy/help/man_help.sh
+++ wrf_deploy/help/man_help.sh
@@ -6,7 +6,7 @@
 printf "%bscript usage:%b ./deploy_wrf.sh [parameter]\\n" "${GREEN}" "${NC}"
 printf "%bDeploy parameter:\\n%b" "${YELLOW}" "${NC}"
 printf "%b     --help      %b   show help text\\n" "${LIGHT_BLUE}" "${NC}"
-printf "%b     --default   %b   runs the deployment with default settings WRF 4.6 low data\\n" "${LIGHT_BLUE}" "${NC}"
+printf "%b     --default   %b   runs the deployment with default settings: version with index argument 1 and low data\\n" "${LIGHT_BLUE}" "${NC}"
 printf "%b -v, --version   %b   argument: <index>%b; specifies the wrf version\\n" "${LIGHT_BLUE}" "${RED}" "${NC}"
 printf "                    1: 4.8, 2: 4.7, 3: 4.6, 4: 4.5, 5: 4.4\\n"
 printf "%b -g, --geodata   %b   argument: <index>%b; specifies the wrf geodata\\n" "${LIGHT_BLUE}" "${RED}" "${NC}"
